dadd6cb84739391ead137d99a8988f88b9f11bb5
Ari Romero <ari.romero-contractor@procore.com>
2023-12-15 11:25:06 -0300

    moved to Lazyvim

--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require('cfg.settings')
-require('cfg.keymaps')
-require('cfg.plugins')
-
--- guide from  https://github.com/cpow/cpow-dotfiles/blob/master/init.lua
---
+++ nvim/lua/plugins/custom.lua
@@ -0,0 +1,32 @@
+return {
+  { "joshdick/onedark.vim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+  { "folke/trouble.nvim", enabled = false },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "ruby",
+      },
+    },
+  },
+}
