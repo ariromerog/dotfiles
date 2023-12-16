0d7b821d15002d1f0fa9d18eca94a1074db78ace
Ari Romero <ari.romero@gmail.com>
2023-12-15 22:37:25 -0300

    use warmer variant

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -1,32 +1,38 @@
 return {
-  { "joshdick/onedark.vim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-    },
-  },
-  { "folke/trouble.nvim", enabled = false },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "ruby",
-      },
-    },
-  },
+	{
+		"navarasu/onedark.nvim",
+		lazy = true,
+		opts = {
+			style = "warmer",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "onedark",
+		},
+	},
+	{ "folke/trouble.nvim", enabled = false },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				"ruby",
+			},
+		},
+	},
 }
