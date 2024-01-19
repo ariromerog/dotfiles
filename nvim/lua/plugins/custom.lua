b8ad6b6e57af7a6fd2635fa21df885e16c58be2d
Ari Romero <ari.romero@gmail.com>
2024-01-19 10:30:04 -0300

    no idea

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -1,40 +1,49 @@
 return {
-	{
-		"navarasu/onedark.nvim",
-		lazy = true,
-		opts = {
-			style = "warmer",
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "onedark",
-		},
-	},
-	{ "folke/trouble.nvim", enabled = false },
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-				"ruby",
-				"rust",
-				"c",
-			},
-		},
-	},
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    opts = {
+      style = "warmer",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+  { "folke/trouble.nvim", enabled = false },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "ruby",
+        "rust",
+        "c",
+      },
+    },
+  },
+  {
+    "nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+        signs = true,
+      },
+    },
+  },
 }
