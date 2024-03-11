62f345f0e2d64ddd15499a6b49871731e5052c1e
Ari Romero <ari.romero-contractor@procore.com>
2024-03-11 16:26:02 -0300

    update neotree and tmux navigator

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -1,45 +1,68 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    lazy = true,
-    opts = {
-      style = "warmer",
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-    },
-  },
-  { "folke/trouble.nvim", enabled = false },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "ruby",
-        "rust",
-        "c",
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = false,
-  },
-  { "tpope/vim-fugitive" },
+	{
+		"navarasu/onedark.nvim",
+		lazy = true,
+		opts = {
+			style = "warmer",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "onedark",
+		},
+	},
+	{ "folke/trouble.nvim", enabled = false },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				"ruby",
+				"rust",
+				"c",
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		enabled = false,
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		follow_current_file = {
+			enabled = false,
+			leave_dirs_open = false,
+		},
+	},
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+	},
 }
