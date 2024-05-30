9da5ccf3a092a9f7478f0d40ecc5d5f9f6bb3fdd
Ari Romero <ari.romero@gmail.com>
2024-05-30 12:09:16 -0400

    LuaLine requires Trouble?

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -1,45 +1,44 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    lazy = true,
-    opts = {
-      style = "warmer",
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-    },
-  },
-  { "folke/trouble.nvim", enabled = false },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "ruby",
-        "rust",
-        "c",
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = false,
-  },
-  { "tpope/vim-fugitive" },
+	{
+		"navarasu/onedark.nvim",
+		lazy = true,
+		opts = {
+			style = "warmer",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "onedark",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				"ruby",
+				"rust",
+				"c",
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		enabled = false,
+	},
+	{ "tpope/vim-fugitive" },
 }
