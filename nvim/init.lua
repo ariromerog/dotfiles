b51a86009578a86f11b1ac1116f1ea5dbe1daf14
Ari Romero <ari.romero-contractor@procore.com>
2024-09-13 20:43:48 -0300

    moved lazy lock

--- nvim/init.lua
+++ nvim/init.lua
@@ -470,15 +470,6 @@ require("lazy").setup({ -- Detect tabstop and shiftwidth automatically
 		end,
 	},
 
-	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
-
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
-	--  Here are some example plugins that I've included in the Kickstart repository.
-	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
-	--
 	require("kickstart.plugins.telescope"),
 	require("kickstart.plugins.which-key"),
 	require("kickstart.plugins.debug"),
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+	},
+}
